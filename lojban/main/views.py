3acb5e9902ed315b4f1864241458dcfe19b08210
Jim Dabell <jim@git.dabell.name>
2008-04-17 23:53:37 +0100

    Initial search functionality.

--- lojban/main/views.py
+++ lojban/main/views.py
@@ -2,7 +2,7 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-from lojban.main.models import NewsItem
+from lojban.main.models import *
 
 def home(request):
     news_items = NewsItem.objects.all()[:2]
@@ -13,7 +13,23 @@ def news(request):
     return render_to_response("news.html", {"news_items": news_items}, context_instance=RequestContext(request))
 
 def search(request):
-    return render_to_response("search.html", context_instance=RequestContext(request))
+    keywords = request.GET.get("keywords", "").split()
+    matches = []
+    for keyword in keywords:
+        keyword = keyword.replace(".", " ").strip()
+        for Valsi in Gismu, Cmavo, Lujvo, Fuhivla:
+            try:
+                valsi = Valsi.objects.get(name=keyword)
+                valsi_type = type(valsi).__name__.lower()
+                if valsi_type == "fuhivla":
+                    valsi_type = "fu'ivla"
+                autolink_regexp = re.compile(r'\{([a-zA-Z\']+)\}')
+                valsi.notes = valsi.notes.replace("&", "&amp;").replace("<", "&lt;")
+                valsi.notes = autolink_regexp.sub(r'<a href="/search/?keywords=\1">\1</a>', valsi.notes)
+                matches.append((valsi, valsi_type))
+            except Valsi.DoesNotExist:
+                pass
+    return render_to_response("search.html", {"matches": matches}, context_instance=RequestContext(request))
 
 
 
